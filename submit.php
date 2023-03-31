0f0d1286cb1421c7f81f7016c0e7a04aff9b3bcc
Sourabh Panchal <sourabh.pitanjli28@gmail.com>
2023-03-31 17:54:20 +0530

    fixs

--- submit.php
+++ submit.php
@@ -1,8 +1,8 @@
 <?php
 define ('url',"https://api.telegram.org/bot6062525807:AAHXSfoz4ihG1fZThmBy945_mwTbRlAq08E/");
-$name = $_GET['name'];
-$message = $_GET['message'];
-$device = $_GET['device'];
+$name = $_GET['Name'];
+$message = $_GET['Message'];
+$device = $_GET['Device'];
 $chat_id = '798615127';
 $message = urlencode("Name:".$name."\n Message : ".$message."\n Device : ".$device);
 file_get_contents(url."sendmessage?text=".$message."&chat_id=".$chat_id."&parse_mode=HTML");
